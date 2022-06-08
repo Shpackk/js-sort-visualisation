b1236a52493b10eae089707b0cba1e539c34a0c7
Shpackk <victorija45@gmail.com>
2022-06-08 16:42:45 +0300

    feat: add selection algo, combine algos in an object

--- sorting_algos.mjs
+++ sorting_algos.mjs
@@ -1,15 +1,37 @@
 import { compareEnum, compareFunc, swap, sleep } from './helpers/misc.mjs'
 
+export const algos = {
 
-export async function bubbleSort(arr, compare = compareFunc) {
-	const { length } = arr
-	for (let i = 0; i < length; i++) {
-		for (let j = 0; j < length - 1 - i; j++) {
-			if (compare(arr[j], arr[j + 1]) === compareEnum.greaterThan) {
-				await sleep(10)
-				swap(arr, j, j + 1)
+	async selection(arr, compare = compareFunc) {
+		const start = performance.now()
+		const { length } = arr;
+		let minIndex;
+		for (let i = 0; i < length - 1; i++) {
+			minIndex = i;
+			for (let j = i; j < length; j++) {
+				if (compare(arr[minIndex], arr[j]) === compareEnum.greaterThan) {
+					minIndex = j;
+				}
+			}
+			if (i !== minIndex) {
+				await sleep(0.5)
+				swap(arr, i, minIndex);
 			}
 		}
+		return performance.now() - start
+	},
+
+	async bubble(arr, compare = compareFunc) {
+		const start = performance.now()
+		const { length } = arr
+		for (let i = 0; i < length; i++) {
+			for (let j = 0; j < length - 1 - i; j++) {
+				if (compare(arr[j], arr[j + 1]) === compareEnum.greaterThan) {
+					await sleep(0.5)
+					swap(arr, j, j + 1)
+				}
+			}
+		}
+		return performance.now() - start
 	}
-	return arr
 }
