2819b24d34991a617d6269664074138d2ec2782d
Shpackk <victorija45@gmail.com>
2022-06-08 16:43:09 +0300

    feat: add time logger

--- helpers/htmlWork.mjs
+++ helpers/htmlWork.mjs
@@ -1,14 +1,22 @@
 const formElement = document.getElementById('header-method-selector')
 
 export function logBigO(algo) {
-	const bigOInfo = document.createElement('p')
+	const bigOInfo = document.createElement('h1')
 	switch (algo) {
 		case 'bubble':
 			bigOInfo.textContent = "Best: O(N), Worst: O(N^2)"
-			formElement.appendChild(bigOInfo)
 			break;
-	
+		case 'selection':
+			bigOInfo.textContent = "Best/Worst: O(N^2)"
+			break;
 		default:
 			break;
 	}
+	formElement.appendChild(bigOInfo)
+}
+
+export function logTime( time ) {
+	const timeInfo = document.createElement('h2')
+	timeInfo.textContent = Math.round(time) + 'ms'
+	formElement.appendChild(timeInfo)
 }
